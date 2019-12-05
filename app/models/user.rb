73e818bd67f9018068deaea35a730425ebc9574e
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-05 03:07:11 -0800

    Added name params for user signup/edit account

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  private 
+  
+    def sign_up_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
