2d4c8788d36da0ae1408bbfc90819ef66da0ddc7
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-12-06 01:35:28 -0800

    Added functionality of updating and deleting products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  
   def index 
     @products = Product.all
+  end
+
+  def show
   end
 
   def new 
@@ -12,9 +17,26 @@ class ProductsController < ApplicationController
     @product.save ? (redirect_to products_path) : (render 'new')
   end
 
+  def edit     
+  end
+
+  def update
+    @product.update(product_params)
+    @product.save ? (redirect_to products_path) : (render 'edit')
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :color, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
